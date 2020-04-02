3c6a0d138fbf9853ed4dfbc231166fc6b3288ed6
studTon <bra.17.ess@gmail.com>
2020-04-02 15:50:02 -0300

    Create main.c

---
+++ Console calculator/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+main()
+{
+    int num, i, op;
+    do
+    {
+       puts("Type the option.(0)Exit|(1)Addition|(2)Subtraction|(3)Multiplication|(4)Division");
+       scanf("%d", &op);
+    } while (op!=0);
+    
+}
