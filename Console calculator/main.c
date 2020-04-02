5d782de3bad44b21e21c0cc1d48daeca87a66213
studTon <bra.17.ess@gmail.com>
2020-04-02 16:01:32 -0300

    Update main.c

--- Console calculator/main.c
+++ Console calculator/main.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
 main()
 {
+    char option //secondary option
     int num, i, op;
+    double numAd, sum=0;
     do
     {
        puts("Type the option.(0)Exit|(1)Addition|(2)Subtraction|(3)Multiplication|(4)Division");
        scanf("%d", &op);
+       switch (op)
+       {
+       case 1:{
+           do{puts("Type a number");
+           scanf("%f", &numAd);
+           sum+=numAd;
+           puts("Do you want to continue.");
+           scanf("%c", &option);
+           }while(option!='n');
+
+       }
+           break;
+       
+       default:
+           break;
+       }
     } while (op!=0);
     
 }
