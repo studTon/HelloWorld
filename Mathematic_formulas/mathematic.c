5382d26a224b1a25fe7a0d135ea6c9eeec3fb524
studTon <bra.17.ess@gmail.com>
2020-06-05 21:40:31 -0300

    Update mathematic.c

--- Mathematic_formulas/mathematic.c
+++ Mathematic_formulas/mathematic.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 int main(){
+	int op; //option key
+	int a, b, c;
+	int area;
 	do{
 		
-	}while()
+		printf("Do you want to exit?(Y-yes|N-no)\n");
+		scanf("%d", &op);
+		switch(op){
+			case 'Y': op = 0; break;
+			case 'y': op = 0; break;
+			case 'N': op = 1; break;
+			case 'n': op = 1; break;
+			default: printf("Invalid command\n");
+		}
+	}while(op!=0)
 	return 0;
 }
