7ba77d4cb76060cf3348b018072ba9c90cec8f24
studTon <bra.17.ess@gmail.com>
2020-06-09 20:50:27 -0300

    Better output

--- Matrix multiplication.c
+++ Matrix multiplication.c
@@ -1,4 +1,5 @@
  //Matrix multiplication
+ #include <stdio.h>
  int main(){
  	int matA[3][3], matB[3][3], matR[3][3];
  	int i, j, k;
@@ -23,12 +24,29 @@
 			}
 		}
 	}
+	//Loops showing matrix A and matrix B
+	printf("\nMatrix A\n");
+	for(i = 0; i < 3; i++){
+ 		for(j = 0; j < 3; j++){
+ 			printf("%4d", matA[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\nMatrix B\n");
+	for(i = 0; i < 3; i++){
+ 		for(j = 0; j < 3; j++){
+ 			printf("%4d", matB[i][j]);
+		}
+		printf("\n");
+	}
  	//Loop showing the results
+ 	printf("\nResult Matrix:\n");
  	for(i = 0; i < 3; i++){
  		for(j = 0; j < 3; j++){
  			printf("%4d", matR[i][j]);
-			}
 		}
+		printf("\n");
 	}
+	
  	return 0;
  }
