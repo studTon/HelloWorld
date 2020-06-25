ca93f94dca8baedf769afea53d1036154d1bcb29
studTon <bra.17.ess@gmail.com>
2020-06-25 13:50:31 -0300

    finished code

--- C++ exercises/major_num.cpp
+++ C++ exercises/major_num.cpp
@@ -8,10 +8,23 @@ int main(){
 	if(numA > numB && numA > numC)
 		cout << numA << " is the major number." << endl;
 	else 
-		if(numB > numA && numB > numC)
-			cout << numB << " is the major number." << endl;
-		else
-			if(numC > numB && numC > numA)
-				cout << numC << " is the major number"
+	if(numB > numA && numB > numC)
+		cout << numB << " is the major number." << endl;
+	else
+	if(numC > numB && numC > numA)
+		cout << numC << " is the major number" << endl;
+	else
+	if(numA == numB && numA > numC)
+		cout << numA << " and " << numB << " are majors" << endl;
+	else
+	if(numA == numC && numC > numB)
+		cout << numA << " and " << numC << " are majors" << endl;
+	else
+	if(numC == numB && numB > numA)
+		cout << numB << " and " << numC << " are majors" << endl;
+	else
+	if(numA == numB && numB == numC)
+		cout << "All numbers are equal" << endl;
+					
 	return 0;
 }
