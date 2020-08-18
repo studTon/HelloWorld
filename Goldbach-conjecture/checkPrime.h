8f9a491801340b20f4d12a00119392040cce2726
studTon <bra.17.ess@gmail.com>
2020-08-18 18:02:21 -0300

    Optimization of Goldbach's conjecture algorithm. I added a function to check prime numbers. I increased the list until the number 2000.

---
+++ Goldbach-conjecture/checkPrime.h
@@ -0,0 +1,14 @@
+
+//Function that checks if a number is prime number
+int prime(int num){
+	int div;
+	int i;
+	div = 1;
+	i = 0;
+	while(div <= num){
+		if(num % div == 0)
+			i++;
+		div++;
+	}
+	return i;
+}
